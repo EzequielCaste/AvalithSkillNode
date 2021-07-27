3e5aed76f7f5e91e45df808a04d48c6dba2b8e28
EzequielCaste <ezequiel.caste@gmail.com>
2021-07-27 20:35:25 -0300

    crear un endpoint para texto

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.get('/fecha', (req, res) => {
   res.send(`La fecha de hoy es: ${new Date().toUTCString().substring(0, 16)}`);
+});
+
+app.get('/texto', (req, res) => {
+  try {
+    const texto = fs.readFileSync('./texto.txt', 'utf-8');
+    res.send(texto);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(3000, console.log('Listo!'));
