a14d81319799eb441cc65855a9170732c8a9a3c7
EzequielCaste <ezequiel.caste@gmail.com>
2021-07-27 21:35:56 -0300

    usar asincronismo para leer el archivo

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const fs = require('fs').promises;
+
+app.use(express.json());
 
 app.get('/fecha', (req, res) => {
   res.send(`La fecha de hoy es: ${new Date().toUTCString().substring(0, 16)}`);
 });
 
-app.get('/texto', (req, res) => {
+app.get('/texto', async (req, res) => {
   try {
-    const texto = fs.readFileSync('./texto.txt', 'utf-8');
+    const texto = await fs.readFile('./texto.txt', 'utf-8');
     res.send(texto);
   } catch (error) {
     res.send(error);
